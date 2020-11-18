32f23f400a3fb21aad73b69d92280cca187212e9
Marielle <marielle@widged.com>
2020-11-18 04:39:42 +0100

    conda for python sandbox

--- ml-cet/set-env.sh
+++ ml-cet/set-env.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 # Download the Anaconda3 Install script
-wget -q https://repo.continuum.io/miniconda/Miniconda3-4.6.14-Linux-x86_64.sh -O /tmp/miniconda.sh && \
-    bash /tmp/miniconda.sh -f -b -p /opt/conda && \
-    /opt/conda/bin/conda install --yes && \
-    rm /tmp/miniconda.sh
-
-export PATH=/opt/conda/bin:$PATHclear
