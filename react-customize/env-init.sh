ca0aec46b56960d0dd593bc44cc1a3aafd84cf74
Marielle <marielle@widged.com>
2020-11-27 16:57:49 +0100

    removing not in use

--- react-customize/env-init.sh
+++
@@ -1,8 +0,0 @@
-curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/react-site/pf-react-express.tar.gz > /root/pf-express.tar.gz
-tar -xvzf /root/pf-express.tar.gz
-npm install
-export CHOKIDAR_USEPOLLING=true
-mkdir ~/tutorial/src/components
-touch ./src/app.css
-echo "Starting... this will block the rest of the commands from running..."
-npm start
