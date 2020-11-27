0f7d44dc26deb445e5bef05fa34dae4d6dd52d1d
Marielle <marielle@widged.com>
2020-11-27 15:54:28 +0100

    dashboard

--- react-hackyourfuture-1/set-env.sh
+++ react-hackyourfuture-1/set-env.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Nothing to install, python 3.8.6 is there by default
-# curl -o- -L https://yarnpkg.com/install.sh | bash
+# Nothing to install, yarn 1.22.5 is there by default
 clear
