4915110c2e770ba5d94e78e989b74d1d2abcfaf0
Marielle <marielle@widged.com>
2020-11-27 14:54:15 +0100

    asset

--- react-hackyourfuture-1/assets/06_Data-Visualization/4_histogram-pie.R
+++
@@ -1,8 +0,0 @@
-# Two plot on the same window
-par(mfrow = c(1,2))
-# Histogram
-data <- iris
-hist(data[,2], main = "histogram about sepal width", xlab = "sepal width", ylab = "Frequency")
-# Pie chart
-classes <- summary(data[,5])
-pie(classes, main = "Iris species")
